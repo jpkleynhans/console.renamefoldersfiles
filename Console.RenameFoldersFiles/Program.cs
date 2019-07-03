63919f060adc76583bb07aec8476d1e986327461
JP Kleynhans <jpk@evolvemed.co.za>
2019-07-03 15:54:54 +0200

    Fixes

--- Console.RenameFoldersFiles/Program.cs
+++ Console.RenameFoldersFiles/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Renaming
 {
     class Program
     {
-        private readonly static List<string> mimeTypes = new List<string>() { ".cs", ".sln", ".config", ".txt", ".csproj" };
+        private readonly static List<string> bannedDirectories = new List<string>() { "bin", "obj" };
+        private readonly static List<string> mimeTypes = new List<string>() { ".cs", ".sln", ".config", ".txt", ".csproj", ".user", ".nuspec", ".xdt" };
 
         static void Main(string[] args)
         {
@@ -36,6 +38,7 @@ namespace Renaming
                 Console.WriteLine("Enter word to replace with:");
                 wordToReplaceWith = Console.ReadLine();
             }
+            RenameBaseFiles(directory, wordToReplace, wordToReplaceWith);
             RenameFolder(directory, wordToReplace, wordToReplaceWith);
 
             Console.WriteLine("Press Enter");
@@ -46,13 +49,25 @@ namespace Renaming
             }
         }
 
+        private static void RenameBaseFiles(string path, string source, string dest)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (FileInfo item in di.GetFiles("*").Where(x => mimeTypes.Contains(x.Extension)))
+            {
+                var code = File.ReadAllText(item.FullName);
+                code = code.Replace(source, dest);
+                File.WriteAllText(item.FullName, code);
+            }
+        }
+
         private static void RenameFolder(string path, string source, string dest)
         {
             //Console.WriteLine("{0} {1} {2}", path, source, dest); 
 
-            DirectoryInfo di = new DirectoryInfo(path);
+            var di = new DirectoryInfo(path);
 
-            foreach (DirectoryInfo si in di.GetDirectories("*", SearchOption.TopDirectoryOnly))
+            foreach (DirectoryInfo si in di.GetDirectories("*", SearchOption.TopDirectoryOnly).Where(x => !bannedDirectories.Contains(x.Name)))
             {
                 RenameFiles(si.FullName, source, dest);
                 RenameFolder(si.Parent.FullName + @"\" + si.Name, source, dest);
@@ -81,11 +96,23 @@ namespace Renaming
 
                 if (si.Name.Contains(source) && mimeTypes.Contains(si.Extension))
                 {
+                    RenameFileContents(fileUrl, source, dest);
                     File.Move(fileUrl, $"{path}\\{fileName.Replace(source, dest)}");
+                }
+                else if (mimeTypes.Contains(si.Extension))
+                {
+                    RenameFileContents(fileUrl, source, dest);
                 }
 
                 Console.WriteLine("{0} renamed to {1}", si.FullName, si.Name);
             }
+        }
+
+        private static void RenameFileContents(string path, string source, string dest)
+        {
+            var code = File.ReadAllText(path);
+            code = code.Replace(source, dest);
+            File.WriteAllText(path, code);
         }
     }
 }
